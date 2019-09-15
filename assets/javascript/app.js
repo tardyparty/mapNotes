573b5b92e6e2ef9fa0284ae49db9272166aa8837
tardyparty <tardyparty@users.noreply.github.com>
2019-09-14 22:36:57 -0500

    html and click funcs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,30 @@ var style = "mapbox://styles/calendre/cjzao9s7z0hm71cpdr14cju6z";
 
 var token = "pk.eyJ1IjoiY2FsZW5kcmUiLCJhIjoiOFFtdjVkYyJ9.o5HtQCVfuOeDTxFWe884zA";
 
+var imgURL;
+
 // display map
 mapboxgl.accessToken = token;
 var map = new mapboxgl.Map({
-container: 'mapView', // container id
-style: style, // stylesheet location
-center: [40, -74], // starting position [lng, lat]
-zoom: 15, // starting zoom
-});
+    container: 'mapView', // container id
+    style: style, // stylesheet location
+    center: [40, -74], // starting position [lng, lat]
+    zoom: 15, // starting zoom
+    });
 
-40.723660
+
+var secondMap = new mapboxgl.Map({
+    container: 'imgPreview', // container id
+    style: style, // stylesheet location
+    center: [40.002, -74.003], // starting position [lng, lat]
+    zoom: 15, // starting zoom
+    });
+
+
+$("#submitURL").on("click", function(e) {
+    e.preventDefault();
+
+    imgURL = $("#submitURL").val().trim();
+    console.log(imgURL);
+
+});
