8e25964aa4d056144a096aac010d40424fefaed2
tardyparty <tardyparty@users.noreply.github.com>
2019-09-27 14:12:10 -0500

    got coordinates from map zoom level to place in image obj

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,48 @@
 var style = "mapbox://styles/calendre/cjzao9s7z0hm71cpdr14cju6z";
-
 var token = "pk.eyJ1IjoiY2FsZW5kcmUiLCJhIjoiOFFtdjVkYyJ9.o5HtQCVfuOeDTxFWe884zA";
+var imgURL = "";
+var center = [-73.997744, 40.730907];
+var bounds;
 
-var imgURL;
+var overlay = 
+    {"overlay": {
+        "type": "vector",
+        "url": imgURL,
+        "coordinates": bounds,
+    }
+}
 
 // display map
 mapboxgl.accessToken = token;
 var map = new mapboxgl.Map({
     container: 'mapView', // container id
     style: style, // stylesheet location
-    center: [40, -74], // starting position [lng, lat]
-    zoom: 15, // starting zoom
-    });
-
-
-var secondMap = new mapboxgl.Map({
-    container: 'imgPreview', // container id
-    style: style, // stylesheet location
-    center: [40.002, -74.003], // starting position [lng, lat]
-    zoom: 15, // starting zoom
+    center: center, // starting position [lng, lat]
+    zoom: 12, // starting zoom
     });
 
 
 $("#submitURL").on("click", function(e) {
     e.preventDefault();
 
-    imgURL = $("#submitURL").val().trim();
+    imgURL = $("#imgURL").val().trim();
     console.log(imgURL);
 
+    $("#imgURL").val("");
+
 });
+
+console.log(imgURL);
+
+map.on('idle', function (e) {
+
+    // save new boundaries
+    bounds = map.getBounds();
+
+    
+    console.log(bounds);
+});
+
+map.addLayer({
+    
+})
---
+++ codeArt.js
@@ -0,0 +1,21 @@
+// Do I need a nap?
+
+var napped = false;
+
+function takeNap() {
+    return pillow + couch 
+}
+
+function stressed() {
+    if (napped = false){
+    takeNap();
+    }
+    else {
+        return "Take a nap anyway, you deserve it."
+    }
+}
+
+
+
+
+
